5a24be8340f05c8e5dc40bb65e45b6273117f0be
Soumya <soumyadipta0077@gmail.com>
2020-11-02 13:07:51 +0530

    [Soumya] Add: Adding Same Name Twice In Table

---
+++ PayrollService/uc10_AddingSameNameTwiceInTable.sql
@@ -0,0 +1,5 @@
+update employee_payroll set department='Sales' where name='Teresa'
+insert into employee_payroll(name,basic_pay,start,gender,department,Deduction,taxable_pay,IncomeTax,Netpay)values
+(
+'Teresa',200000.00,'2019-11-13','F','Marketing',100000.0,200000.0,50000.0,150000.0
+)
